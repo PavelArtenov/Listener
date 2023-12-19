ddd199fbb15a982466e80f151f1811d9b011c7fb
PavelArtenov <myemail@mail.com>
2023-12-19 16:50:37 +0300

    121

--- src/main/java/com/app/listener1/controller/RequestController.java
+++ src/main/java/com/app/listener1/controller/RequestController.java
@@ -2,13 +2,14 @@ package com.app.listener1.controller;
 
 import com.app.listener1.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
 
-    @GetMapping("listener/save")
+    @PostMapping("listener/save")
     public void saveRequest(@RequestBody Model request) {
         System.out.println(request.getId());
     }
